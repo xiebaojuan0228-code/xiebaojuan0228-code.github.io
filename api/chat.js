a94345f0e297a06635a09a02f9547ae9ea42c2f3
xiebaojuan0228-code <xiebaojuan0228@gmail.com>
2026-06-15 19:23:57 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -26,11 +26,11 @@ export default async function handler(req, res) {
             }
         );
 
-        const data = await response.json();
+        const data = await response.text();
 
-        return res.status(200).json({
-            reply: data.choices?.[0]?.message?.content || "没有获取到回复"
-        });
+return res.status(200).json({
+    reply: data
+});
 
     } catch (error) {
     return res.status(500).json({
